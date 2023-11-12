c3bc167a96047324689344ea023fd345b0ddbc51
robsondmf1234@gmail.com <Itupeva123@>
2023-11-12 20:04:39 -0300

    feat - Fazendo exercicio Alura.

--- app/src/main/java/com/example/jetpackcomposeestudos/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposeestudos/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -17,6 +18,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -26,7 +29,9 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
 import androidx.compose.ui.unit.dp
 import com.example.jetpackcomposeestudos.ui.components.ChallengeComposable
 import com.example.jetpackcomposeestudos.ui.theme.JetPackComposeEstudosTheme
@@ -39,13 +44,13 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetPackComposeEstudosTheme {
-                ProductItem()
+                exerciseCompose()
             }
         }
     }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
 fun ProductItem() {
     Column(
@@ -75,6 +80,44 @@ fun ProductItem() {
         )
         Text(text = "Texto 1")
         Text(text = "Texto 2")
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun exerciseCompose() {
+    Surface(
+        Modifier.padding(0.dp),
+        shape = RoundedCornerShape(12.dp),
+        shadowElevation = 4.dp
+    ) {
+        Row(
+            modifier = Modifier
+                .height(150.dp)
+                .fillMaxWidth()
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .width(100.dp)
+                    .background(
+                        brush = Brush.verticalGradient(
+                            colors = listOf(Teal200, Purple500)
+                        )
+                    )
+            )
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_background),
+                contentDescription = null,
+                Modifier
+                    .size(100.dp)
+                    .offset(x = ((-50).dp))
+                    .align(Alignment.CenterVertically)
+                    .clip(shape = CircleShape)
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            Text(text = LoremIpsum(50).values.first(), maxLines = 6, modifier = Modifier.align(Alignment.CenterVertically), overflow = TextOverflow.Ellipsis)
+        }
     }
 }
 
